25430e29c28d4c5dafc1456d6da2cb0123609f54
Adam Oakes <amot92@mail.missouri.edu>
2018-02-02 20:21:41 -0600

    Finished

--- Challenge 2/main.swift
+++ Challenge 2/main.swift
@@ -22,5 +22,61 @@ let integral:Character = "\u{222B}"
 let greeting = "Hello"
 var name = "Karen"
 
+if(sample1 == sample2){
+    print("The Samples are equal.")
+}else{
+    print("The Samples are not equal.")
+}
 
+if(heartRate >= 40 && heartRate <= 80){
+    print("Heart rate is normal.")
+}else{
+    print("Heart rate is not normal.")
+}
 
+if(deposits >= 100000000){
+    print("You are exceedingly wealthy.")
+}else{
+    print("Sorry you are so poor.")
+}
+
+var force = mass * acceleration
+
+print("force=\(force)")
+
+print("\(distance) is the distance.")
+
+if(lost){
+    if(expensive){
+        print("I am really sorry! I will get the manager.")
+    }else{
+        print("Here is a coupon for 10% off.")
+    }
+}
+
+switch choice {
+case 1:
+    print("You chose 1.")
+case 2:
+    print("You chose 2.")
+case 3:
+    print("You chose 3.")
+default:
+    print("You made an unknown choice.")
+}
+
+print("\(integral) is an integral")
+
+for var i in 0...5{
+    print("i = \(i)")
+    
+}
+
+var age = 0
+
+while age < 6 {
+    print("age = \(age)")
+    age+=1
+}
+
+print("\(greeting) \(name)")
