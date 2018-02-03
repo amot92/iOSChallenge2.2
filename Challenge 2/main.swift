8a1a01e330b2bcd2c95d967aa2468412ca8db3cb
Adam Oakes <amot92@mail.missouri.edu>
2018-02-02 19:34:54 -0600

    Fist Commit

---
+++ Challenge 2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Challenge 2
+//
+//  Created by Adam Oakes on 2/2/18.
+//  Copyright © 2018 Adam Oakes. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
